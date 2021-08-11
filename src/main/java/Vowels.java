ad13269c3876306996f6f508bac0a1f14ee6d10c
xiyu.liang <1595345149@qq.com>
2021-08-11 18:41:58 +0800

    [liang xi yu & wang xin huan] create isMoreThan30 method

--- src/main/java/Vowels.java
+++ src/main/java/Vowels.java
@@ -1,5 +1,9 @@
 public class Vowels {
     public boolean isVowels(String testLetter) {
+        return testLetter.matches("[aeiou]+");
+    }
+
+    public boolean isMoreThan30(String testLetter) {
         return true;
     }
 }
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -9,4 +9,18 @@ public class MommifierTest {
         String testLetter = "a";
         Assertions.assertTrue(vowels.isVowels(testLetter));
     }
+
+    @Test
+    void should_return_if_given_are_vowels(){
+        Vowels vowels = new Vowels();
+        String testLetter = "aei";
+        Assertions.assertTrue(vowels.isVowels(testLetter));
+    }
+
+    @Test
+    void should_return_if_vowels_count_more_than_30(){
+        Vowels vowels = new Vowels();
+        String testLetter = "aeihj";
+        Assertions.assertTrue(vowels.isMoreThan30(testLetter));
+    }
 }
