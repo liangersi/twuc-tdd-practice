34a875b37293dd2fc0c2abf79916823c45a23c52
xiyu.liang <1595345149@qq.com>
2021-08-11 15:58:46 +0800

    [liangxiyu&wangxinhuan] create vowels class

--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -1,2 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MommifierTest {
+
+    @Test
+    void should_return_if_given_is_vowels(){
+        Vowels vowels = new Vowels();
+    }
 }
